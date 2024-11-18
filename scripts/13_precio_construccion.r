13a63c1704ce9a9c34f2ca2fb1c31c2413604a81
Pedro Salas-Rojo <p.salas-rojo@lse.ac.uk>
2024-11-18 15:43:37 +0000

    Update 13_precio_construccion.r

--- scripts/13_precio_construccion.r
+++ scripts/13_precio_construccion.r
@@ -1,18 +1,1 @@
-#         Author: Pedro Salas Rojo
-#         Date: 11/2024
-#         Name of project: Clean Indices Precios de Construcción
-#         Data: Estadísticos del Índice de Precios de Construcción, INE
-#         Serie origen: https://www.ine.es/dyngs/INEbase/es/operacion.htm?c=Estadistica_C&cid=1254736154972&idp=1254735576757
-
-rm(list = ls(all.names = TRUE)) 
-library(tidyverse)
-library(readxl)
-
-name="Pedro"
-
-if (name=="Pedro"){
-  path <- paste0("C:/Users/user/Documents/mispapers/Housing/data/")
-} else {
-  path <- paste0("H:/")
-}
 
